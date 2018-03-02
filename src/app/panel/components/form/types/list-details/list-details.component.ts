b194d09439da63e77d4988836249d1b68580228d
damiandominella <damiandominella@gmail.com>
2018-03-02 15:44:59 +0100

    drag&drop

--- src/app/panel/components/form/types/list-details/list-details.component.ts
+++ src/app/panel/components/form/types/list-details/list-details.component.ts
@@ -5,18 +5,16 @@ import {FormArray, FormGroup} from '@angular/forms';
 import {FormGeneratorService} from '../../../../services/form-generator.service';
 
 @Component({
-  selector: 'app-list-details',
-  templateUrl: './list-details.component.html',
-  styleUrls: ['./list-details.component.scss']
+    selector: 'app-list-details',
+    templateUrl: './list-details.component.html',
+    styleUrls: ['./list-details.component.scss']
 })
 export class ListDetailsComponent extends BaseInputComponent implements OnInit {
 
     @Input() field: FormFieldListDetails;
 
-    private subForms: any[] = [];
-
     constructor(private _formGenerator: FormGeneratorService) {
-      super();
+        super();
     }
 
     ngOnInit() {
@@ -27,14 +25,12 @@ export class ListDetailsComponent extends BaseInputComponent implements OnInit {
     }
 
     addDetail() {
-        this.subForms.push('item');
         // add new formgroup
         this.getControl().push(new FormGroup(this._formGenerator.generateFormFields(this.field.fields)));
     }
 
     deleteDetail(index: number) {
         // remove the chosen row
-        this.subForms.splice(index, 1);
         this.getControl().removeAt(index);
     }
 }
