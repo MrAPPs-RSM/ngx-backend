fd9f6d4769382ddb9161cbe58237120fb67818ff
damiandominella <damiandominella@gmail.com>
2018-03-21 12:43:17 +0100

    removed useless code

--- src/app/panel/components/form/interfaces/form-field-preview.ts
+++ src/app/panel/components/form/interfaces/form-field-preview.ts
@@ -3,6 +3,4 @@ import {FormField} from './form-field';
 export interface FormFieldPreview extends FormField {
     fileKey?: any; // file upload field key
     endpoint?: string; // endpoint to call to get preview
-    width?: number;
-    height?: number;
 }
