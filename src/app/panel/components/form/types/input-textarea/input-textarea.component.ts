439e3140a9defc5ab38e257d36c2e383d2340d17
damiandominella <damiandominella@gmail.com>
2018-03-29 11:07:56 +0200

    new editor

--- src/app/panel/components/form/types/input-textarea/input-textarea.component.ts
+++ src/app/panel/components/form/types/input-textarea/input-textarea.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormFieldTextarea} from '../../interfaces/form-field-textarea';
 import {BaseInputComponent} from '../base-input/base-input.component';
+import {LanguageService} from '../../../../services/language.service';
 
 
 @Component({
@@ -12,7 +13,37 @@ import {BaseInputComponent} from '../base-input/base-input.component';
 export class InputTextareaComponent extends BaseInputComponent implements OnInit {
 
     @Input() field: FormFieldTextarea;
+    private focus: boolean;
+    config: any = {};
+
+    constructor(private _language: LanguageService) {
+        super();
+        this.focus = false;
+    }
+
+    get isValid() {
+        if (this.getControl().touched && this.focus) {
+            return this.getControl().valid;
+        } else {
+            return true;
+        }
+    }
 
     ngOnInit() {
+        if (this.field.options && this.field.options.editor) {
+            this.config = {
+                language: this._language.getCurrentLang().isoCode,
+                toolbar: [
+                    ['Format', 'Font', 'FontSize', 'Bold', 'Italic', 'Underline', 'StrikeThrough', '-', 'Undo', 'Redo', '-', 'Cut', 'Copy', 'Paste', '-', 'Outdent', 'Indent'],
+                    ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+                    '/',
+                    ['Table', '-', 'Link', 'Smiley', 'TextColor', 'BGColor', 'Source']
+                ]
+            };
+        }
+    }
+
+    onFocus() {
+        this.focus = true;
     }
 }
