ba0b4fe18264016e382926b6f800473fe6f54117
damiandominella <damiandominella@gmail.com>
2018-04-24 12:11:50 +0200

    added id support

--- src/app/panel/components/form/types/gallery/gallery.component.ts
+++ src/app/panel/components/form/types/gallery/gallery.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Gallery} from '../../interfaces/gallery';
 import {ApiService} from '../../../../../api/api.service';
 import {ToastsService} from '../../../../../services/toasts.service';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
     selector: 'app-gallery',
@@ -16,7 +17,9 @@ export class GalleryComponent implements OnInit {
     isLoading: boolean;
     gallery: Media[];
 
-    constructor(private _apiService: ApiService, private _toast: ToastsService) {
+    constructor(private _apiService: ApiService,
+                private _toast: ToastsService,
+                private _route: ActivatedRoute) {
     }
 
     ngOnInit() {
@@ -26,7 +29,11 @@ export class GalleryComponent implements OnInit {
 
     private loadGallery(): void {
         this.isLoading = true;
-        this._apiService.get(this.field.options.endpoint).then((data) => {
+        let endpoint = this.field.options.endpoint;
+        if (endpoint.indexOf(':id') !== -1) {
+            endpoint = endpoint.replace(':id', this._route.snapshot.params['id']);
+        }
+        this._apiService.get(endpoint).then((data) => {
             this.isLoading = false;
             this.gallery = data;
         }).catch((err) => {
