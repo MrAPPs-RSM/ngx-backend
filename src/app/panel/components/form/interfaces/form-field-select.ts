14ccac3e2448716d1770f2b9dcdd63facf5fcdcd
Samuele Mazza <samuele@mr-apps.com>
2018-05-09 10:52:31 +0200

    added addTag property to enable ng-select adding tags not downloaded from webservice

--- src/app/panel/components/form/interfaces/form-field-select.ts
+++ src/app/panel/components/form/interfaces/form-field-select.ts
@@ -3,6 +3,7 @@ import {FormField} from './form-field';
 export interface FormFieldSelect extends FormField {
     options: any;
     multiple?: boolean;
+    addTag?: boolean;
     lang?: boolean; // if lang needed
     dependsOn?: any; // if options of the select depends on a field of the current form
 }
---
+++ src/environments/environment.dev.colombini.ts
@@ -0,0 +1,106 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment: any = {
+    production: false,
+    languages: [
+        {
+            name: 'Italiano',
+            isoCode: 'it'
+        },
+        {
+            name: 'English',
+            isoCode: 'en'
+        }
+    ],
+    currentLang: 'en',
+    name: 'Ngx Backend',
+    assets: {
+        logo: '/assets/images/logo.png',
+        imageError: '/assets/images/image-error.png'
+    },
+    api: {
+        baseUrl: 'http://0.0.0.0:5555/api/',
+        setupEndpoint: 'setup'
+    },
+    auth: {
+        tokenKey: 'access_token',
+        login: {
+            endpoint: 'persons/login?include=user',
+            form: {
+                title: 'Ngx Backend - Login',
+                class: 'col-sm-12 primary',
+                fields: {
+                    base: [
+                        {
+                            key: 'username',
+                            type: 'text',
+                            label: 'Username',
+                            placeholder: 'Username',
+                            validators: {
+                                required: true
+                            }
+                        },
+                        {
+                            key: 'password',
+                            type: 'password',
+                            label: 'Password',
+                            placeholder: 'Password',
+                            validators: {
+                                required: true,
+                                minLength: 4
+                            }
+                        }
+                    ],
+                },
+                submit: {
+                    label: 'Login'
+                },
+                buttons: [
+                    {
+                        class: 'btn-sm btn-link no-padding',
+                        content: 'Forgot password?',
+                        config: {
+                            path: '/password-reset'
+                        }
+                    }
+                ],
+                isLoginForm: true
+            }
+        },
+        passwordReset: {
+            endpoint: 'password_reset',
+            form: {
+                class: 'col-sm-12 primary',
+                title: 'Ngx Backend - Password reset',
+                fields: {
+                    base: [
+                        {
+                            key: 'username',
+                            type: 'text',
+                            label: 'Username',
+                            placeholder: 'Username',
+                            validators: {
+                                required: true
+                            }
+                        }
+                    ],
+                },
+                submit: {
+                    label: 'Reset password'
+                },
+                buttons: [
+                    {
+                        class: 'btn-sm btn-link no-padding',
+                        content: 'Back to login',
+                        config: {
+                            path: '/login'
+                        }
+                    }
+                ]
+            }
+        }
+    }
+};
